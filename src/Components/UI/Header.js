cba2c9e92e116b64489d22e23a735a361c129edd
wnortier <warwick.nortier@gmail.com>
2021-10-16 15:02:33 +0200

    added elevationscroll component

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -1,12 +1,29 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-
+import useScrollTrigger from "@mui/material/useScrollTrigger";
 const Header = () => {
+  function ElevationScroll(props) {
+    const { children } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+    });
+
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+
   return (
-    <AppBar>
-      <Toolbar>Arc Development</Toolbar>
-    </AppBar>
+    <ElevationScroll>
+      <AppBar>
+        <Toolbar>Arc Development</Toolbar>
+      </AppBar>
+    </ElevationScroll>
   );
 };
 
