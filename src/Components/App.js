6b2d3c675b33a42b64accee471a84c6db783ca57
wnortier <warwick.nortier@gmail.com>
2021-10-16 13:56:47 +0200

    installed material ui

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import Header from "../Components/UI/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/UI/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+
+const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar>Arc Development</Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
